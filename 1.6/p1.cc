3b7a70dba6ce961e612cc7602f1340e3599526ae
qingzhu <q15000557748@gmail.com>
2014-07-24 17:49:50 +0800

    1.0

---
+++ 1.6/p1.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+
+using namspace std;
+
+int a[10000000];
+int n;
+int main(){
+    n = 0;
+    while(scanf("%d", &a[n]) != EOF)n++;
+}
+
